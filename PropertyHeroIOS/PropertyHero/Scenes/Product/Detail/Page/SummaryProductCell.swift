b1774a3c84ea6cfd12d44fa4a9436b6df31fbe75
Khoi Le <khoiln218@gmail.com>
2024-07-31 13:38:17 +0700

    update detail

--- PropertyHeroIOS/PropertyHero/Scenes/Product/Detail/Page/SummaryProductCell.swift
+++ PropertyHeroIOS/PropertyHero/Scenes/Product/Detail/Page/SummaryProductCell.swift
@@ -52,9 +52,8 @@ class SummaryProductCell: PageTableCell {
     func bindViewModel(_ viewModel: Product) {
         labelId.text = "Mã tin: \(viewModel.Id)"
         
-        let priceText:NSMutableAttributedString = NSMutableAttributedString(string: viewModel.PropertyName + "  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(hex: "#607D8B")!])
-        priceText.append(NSMutableAttributedString(string: String(viewModel.Price) + " ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(hex: "#2b50f6")!]))
-        priceText.append(NSMutableAttributedString(string: "tr/thg", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor(hex: "#2b50f6")!]))
+        let priceText:NSMutableAttributedString = NSMutableAttributedString(string: String(viewModel.Price) + " ", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20), NSAttributedString.Key.foregroundColor: UIColor(hex: "#2F2F2F")!])
+        priceText.append(NSMutableAttributedString(string: "triệu / tháng", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor(hex: "#2F2F2F")!]))
         price.attributedText = priceText
         title.text = viewModel.Title
         
